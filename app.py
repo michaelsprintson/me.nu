176f526abb43537b947a1d3ab51055ea026f65fc
Timothy Goh <tmg5@rice.edu>
2019-09-21 03:49:06 -0500

    Added webcam

--- app.py
+++ app.py
@@ -14,14 +14,24 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/saveImage', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'webcam' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files['webcam']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
@@ -30,23 +40,17 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('uploaded_file',
-            #                         filename=filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
     <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
+      <input type=file name=webcam>
       <input type=submit value=Upload>
     </form>
     '''
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
 
 
 @app.route('/foo')
@@ -56,4 +60,5 @@ def foo():
 
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
     app.run(debug=True)
--- static/js/camera.js
+++ static/js/camera.js
@@ -7,34 +7,17 @@ function ShowCam() {
     });
     Webcam.attach('#my_camera');
 }
+
 window.onload= ShowCam;
 
-function snap() {
+function take_snapshot() {
     Webcam.snap( function(data_uri) {
-        // display results in page
-        document.getElementById('results').innerHTML =
-        '<img id="image" src="'+data_uri+'"/>';
-      } );
-}
+        document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
 
-function upload() {
-    console.log("Uploading...")
-    var image = document.getElementById('image').src;
-    var form = document.getElementById('myForm');
-    var formData = new FormData(form);
-    formData.append("file", image);
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "/signup");
-
-    // check when state changes,
-    xmlhttp.onreadystatechange = function() {
-
-    if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        alert(xmlhttp.responseText);
-        }
-    }
-
-    xmlhttp.send(formData);
-    console.log(formData.get('file'));
-    console.log(formData.get('userID'));
+        Webcam.upload( data_uri, '/saveImage', function(code, text) {
+            console.log(code);
+			// 'code' will be the HTTP response code from the server, e.g. 200
+			// 'text' will be the raw response content
+		} );
+    } );
 }
