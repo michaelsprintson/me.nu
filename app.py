423685740e15d8238e32ab2adddd8c623203dd5e
Timothy Goh <tmg5@rice.edu>
2019-09-21 02:54:53 -0500

    Upload file

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def upload():
+    return render_template("index.html")
+
+
+@app.route('/foo', methods=['POST'])
+def success():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        return render_template("foo.html", name=f.filename)
 
 
 @app.route('/foo')
 def foo():
     test = 'asdfasdf'
     return render_template('foo.html', content=test)
-
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        return jsonify(request.form['userID'], request.form['file'])
-    return render_template('signup.html')
 
 
 if __name__ == "__main__":
