0c7510f3ce6c43975d485f6e3f3e14a772a15056
Seung Hun Jang <sj43@rice.edu>
2019-09-22 05:33:35 -0500

    get rid of host

--- app.py
+++ app.py
@@ -116,4 +116,4 @@ if __name__ == "__main__":
     app.secret_key = 'super secret key'
     port = int(os.environ.get("PORT", 5000))
     #host='0,0,0,0',port=port
-    app.run(host='0,0,0,0', port=port)
+    app.run(port=port)
