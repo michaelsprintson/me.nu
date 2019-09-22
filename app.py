a023ab0d16c1a553fe2f718df431e66e475d5a81
Seung Hun Jang <sj43@rice.edu>
2019-09-22 05:28:10 -0500

    change port

--- app.py
+++ app.py
@@ -114,7 +114,6 @@ def suggested_menu():
 
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
-    #port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     #host='0,0,0,0',port=port
-    app.run(host='0,0,0,0', port=80)
-#     , host='0.0.0.0', port=80)
+    app.run(host='0,0,0,0', port=port)
